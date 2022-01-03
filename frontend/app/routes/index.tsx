33ff3d36da7dd93e407dea03a9e36ebd9be8a2dd
Takehiro Shimokawa <takehiro.shimokawa@gmail.com>
2022-01-03 15:02:13 +0900

    git clone https://github.com/remix-run/remix-jokes

---
+++ frontend/app/routes/index.tsx
@@ -0,0 +1,49 @@
+import { Link } from "remix";
+import type { MetaFunction, LinksFunction, HeadersFunction } from "remix";
+import stylesUrl from "../styles/index.css";
+
+export let meta: MetaFunction = () => {
+  return {
+    title: "Remix: So great, it's funny!",
+    description: "Remix jokes app. Learn Remix and laugh at the same time!",
+  };
+};
+
+export let links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+export let headers: HeadersFunction = () => {
+  return {
+    "Cache-Control": `public, max-age=${60 * 10}, s-maxage=${
+      60 * 60 * 24 * 30
+    }`,
+  };
+};
+
+export default function Index() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+            <li>
+              <a href="https://github.com/remix-run/remix-jokes">GitHub</a>
+            </li>
+            <li>
+              <Link reloadDocument to="/jokes.rss">
+                RSS
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
